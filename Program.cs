0ad330ba9ce97838106b59870ac2013dd8e1ee1d
szmuraj <przemyslaw.sicinski@o2.pl>
2025-03-30 08:57:31 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prostokat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat prostokat = new Prostokat(5, 10);
+
+            prostokat.Wypisz();
+        }
+    }
+}
---
+++ Prostokat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostokat
+{
+    class Prostokat
+    {
+        private double dlugosc;
+        private double szerokosc;
+
+        public Prostokat(double dlugosc, double szerokosc)
+        {
+            this.dlugosc = dlugosc;
+            this.szerokosc = szerokosc;
+        }
+
+        public double Powierzchnia()
+        {
+            return dlugosc * szerokosc;
+        }
+
+        public double Obwod()
+        {
+            return 2 * dlugosc + 2 * szerokosc;
+        }
+
+        public void Wypisz()
+        {
+            Console.WriteLine($"Powierzchnia: {Powierzchnia()}\nObwód: {Obwod()}");
+        }
+    }
+}
